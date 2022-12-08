6503595b103f5275ad4ef74043eb058435834a69
Vicky <ying.vicky.cao@outlook.com>
2022-12-08 18:26:08 +0800

    时间复杂度 - O(nlogn) 线性对数阶时间复杂

--- src/main/java/com/hades/example/data_structures_and_algorithms/part1/Example_6.java
+++ src/main/java/com/hades/example/data_structures_and_algorithms/part1/Example_6.java
@@ -6,7 +6,7 @@ public class Example_6 {
     public static void main(String[] args) {
         Example_6 example_6 = new Example_6();
         int result = example_6.cal(5);
-        System.out.println(result);
+        System.out.println(result); // 2
     }
 
     /**
@@ -21,19 +21,22 @@ public class Example_6 {
     private int cal(int n) {
         int i = 1;
         int count_of_unit_time = 0; // 只作为统计，不计入unit_time
+        int result = 0;
 
+        // Tn = O(log 2 n)
         // start
         while (i <= n) {
             i = i * 2;
+            result = count_of_unit_time;
             System.out.println("i=" + i + ",count of unit_time = " + (count_of_unit_time++));
         }
         // end
-        return i;
+        return result;
     }
 
     /**
      * <pre>
-     2^0   2^1   2^2   2^3   2^x = n
+     * 2^0   2^1   2^2   2^3   2^x = n
      * => 2^x = n
      *
      *  这段代码的含义：2^x 小于等于 5， 求X
@@ -44,5 +47,33 @@ public class Example_6 {
      *
      *  这段代码的时间复杂度是Tn = O(log 2 n)
      *  </pre>
+     */
+
+
+    private int cal2(int n) {
+        int i = 1;
+        int count_of_unit_time = 0; // 只作为统计，不计入unit_time
+        int result = 0;
+
+        // Tn = O(log 3 n)
+        // start
+        while (i <= n) {
+            i = i * 3;
+            result = count_of_unit_time;
+            System.out.println("i=" + i + ",count of unit_time = " + (count_of_unit_time++));
+        }
+        // end
+        return i;
+    }
+
+    /**
+     * 不管是 O(log 2 n) 还是 O(log 3 n)，把所有对数阶的时间复杂度都记做 O(log n)。
+     * 为什么？
+     * log 3 n = log(3^2) * log(2 n)
+     * => O(log 3 n) = O (C * log(2 n)),其中C 是 log(3^2)，是一个常量。
+     *
+     * 基于之前的理论： 当采用大O标记复杂度时，可以忽略系数，即O(C(fn)) = O (f(n))
+     * 那么，O(log 3 n)就等于 O (log(2 n)。
+     * 因此，在对数阶时间复杂度的表示方法里，忽略了对数的"底"，统一表示为 log(n)。
      */
 }
--- src/main/java/com/hades/example/data_structures_and_algorithms/part1/Example_7.java
+++ src/main/java/com/hades/example/data_structures_and_algorithms/part1/Example_7.java
@@ -2,4 +2,52 @@ package com.hades.example.data_structures_and_algorithms.part1;
 
 // 时间复杂度 - O(nlogn) 线性对数阶时间复杂
 public class Example_7 {
+
+    public static void main(String[] args) {
+        int[] arrays = new int[]{5, 10};
+
+        Example_7 example_7 = new Example_7();
+        example_7.calArray(arrays);
+    }
+
+    private void calArray(int[] arrays) {
+        int i = 0;
+        int result = 0;
+
+        int n = arrays.length;
+
+        // T1(n) = O(n)
+        // start
+        for (; i < n; i++) {
+            result = cal(arrays[i]);
+
+            System.out.println("array[" + i + "]=" + result);   // 只作为统计，不计入unit_time
+            System.out.println();                               // 只作为统计，不计入unit_time
+        }
+        // end
+    }
+
+    private int cal(int n) {
+        int i = 1;
+        int count_of_unit_time = 0; // 只作为统计，不计入unit_time
+        int result = 0;
+
+        // T2(n) = O(log 2 n)
+        // start
+        while (i <= n) {
+            i = i * 2;
+            result = count_of_unit_time;
+            System.out.println("i=" + i + ",count of unit_time = " + (count_of_unit_time++)); // 只作为统计，不计入unit_time
+        }
+        // end
+        return result;
+    }
+
+    /**
+     * T1(n) = O(n)
+     * T2(n) = O(log 2 n)
+     * 根据乘法原则，那么calArray 整个代码的时间复杂度为 ：
+     * =>T(n) = T1(n) * T2(n) = O(n) * O(log 2 n) = O(n * log 2 n)
+     * => T(n) = O(n * log n)
+     */
 }
